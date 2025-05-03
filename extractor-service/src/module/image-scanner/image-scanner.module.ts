ce24efc54f6dc625058ef63f31b849de7fca849b
Danil Kladnitsky <danyakladnitsky@gmail.com>
2025-05-03 17:16:27 +0300

    refactor: update image scanner module to use pdf-to-img for PDF processing and improve buffer handling

--- extractor-service/src/module/image-scanner/image-scanner.module.ts
+++ extractor-service/src/module/image-scanner/image-scanner.module.ts
@@ -1,32 +1,23 @@
 import Tesseract from "tesseract.js";
-import { fromBuffer } from "pdf2pic"
+import { pdf } from "pdf-to-img";
 
 import type { IImageScannerModule } from "./types";
 
 export const createImageScannerModule = (): IImageScannerModule => {
-    const scanSingleImage = async (imageBuffer: ArrayBuffer) => {
-        const result = await Tesseract.recognize(Buffer.from(imageBuffer), "chi_sim");
+    const scanSingleImage = async (imageBuffer: ArrayBuffer | ArrayBufferLike) => {
+        const buffer = imageBuffer instanceof ArrayBuffer ? imageBuffer : new Uint8Array(imageBuffer).buffer;
+        const result = await Tesseract.recognize(Buffer.from(buffer), "chi_sim");
         return result.data.text;
     }
     return {
         scanImage: scanSingleImage,
         scanPdf: async (pdfBuffer) => {
-            const options = {
-                density: 100,
-                saveFilename: "untitled",
-                savePath: "./images",
-                format: "png",
-                width: 600,
-                height: 600
-              };
-              const convert = fromBuffer(Buffer.from(pdfBuffer), options);
-              const pageToConvertAsImage = 1;
-              
-            const result = await convert(pageToConvertAsImage, { responseType: "image" })
+            const document = await pdf(Buffer.from(pdfBuffer), { scale: 3 });
+            const firstPage = (await document.getPage(99)).buffer
 
-            console.log(result)
+            const text = await scanSingleImage(firstPage);
 
-            return "scanned";
+            return text;
         },
     }
 }
