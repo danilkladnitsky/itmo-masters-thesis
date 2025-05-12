3fe10c066c7e64b204f5fd565074a78e6b177057
Danil Kladnitsky <danyakladnitsky@gmail.com>
2025-05-12 10:33:33 +0300

    fix: update text normalization to retain specific punctuation and newline characters in generated text

--- data-service/src/helpers/normalizeGeneratedText.ts
+++ data-service/src/helpers/normalizeGeneratedText.ts
@@ -1,4 +1,4 @@
 // Оставляем только китайские символы
 export const normalizeGeneratedText = (text: string) => {
-    return text.replace(/[^\u4e00-\u9fa5]/g, "");
+    return text.replace(/[^\u4e00-\u9fa5\\n.,!?]/g, "");
 }
